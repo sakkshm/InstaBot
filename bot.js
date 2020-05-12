c53edf0d9b676e6d0e19a94854090643f32c7607
Saksham Saxena <sakshamappdev@gmail.com>
2020-05-12 11:34:51 +0530

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,72 @@
+const { IgApiClient } = require('instagram-private-api');
+const { get } = require('request-promise');
+var getJSON = require('get-json')
+
+//User Credentials
+const IG_USERNAME = "dankgram.memes";
+const IG_PASSWORD = "dankgram";
+
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+
+(async() => {
+
+    var subreddits = ["memes", "dankmemes"] // list of subreddits 
+    var sub = subreddits[getRandomInt(subreddits.length)] // get a random subreddit
+    var url = "https://meme-api.herokuapp.com/gimme/" + sub;
+
+
+    // IG login
+    const ig = new IgApiClient();
+    ig.state.generateDevice(IG_USERNAME);
+    const auth = await ig.account.login(IG_USERNAME, IG_PASSWORD);
+    console.log(JSON.stringify(auth));
+
+
+    // Get memes
+    getJSON(url, function(error, response) {
+        global.data = response;
+    }).then(function() {
+        console.log(global.data);
+
+
+
+        (async() => {
+
+            var caption = data.title + " \n\n\n \t\t\t Orignal post in " +
+                data.subreddit + " : " +
+                data.postLink +
+                " Follow @dankgram.memes for the most dank memes on Instagram. 🔥🔥🔥 " +
+                "#meme #memes #bestmemes #instamemes #funny #funnymemes #dankmemes #edgymemes #spicymemes #nichememes #memepage #funniestmemes #dank #memesdaily #jokes #memesrlife #memestar #memesquad #humor #lmao #igmemes #lol #memeaccount #memer #relatablememes #funnyposts #sillymemes #nichememe #memetime";
+
+
+            const imageBuffer = await get({
+                url: data.url,
+                encoding: null,
+            });
+
+
+            //publish post
+            const publishResult = await ig.publish.photo({
+                file: imageBuffer,
+                caption: caption
+            });
+
+            console.log(publishResult);
+
+            if (publishResult.status == "ok") {
+                console.log("Posted succesfully!!!")
+            } else {
+                console.log("Erorr in posting to Instagram.....")
+            }
+
+
+        })();
+
+    })
+
+
+})();
